8cbe444ba6d602bc8079cfb74aa6b8f05c950671
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-06 14:34:19 -0500

    Add basic file input form and begin setup of POST /convert route-handler.

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/convert', (req, res, next) => {
+  
+  res.send('ok');
+});
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
