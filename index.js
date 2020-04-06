e0ca53ca23055ffe84fb4578d85e1784580f55f0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-06 15:15:39 -0500

    Convert image to webp and allow user to download.

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const path = require('path');
 const express = require('express');
 const multer  = require('multer')
 const morgan = require('morgan');
+const sharp = require('sharp');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' })
+
+const uploadsDir = path.join(__dirname, 'uploads');
+const convertedDir = path.join(__dirname, 'converted');
 
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/image', express.static(path.join(__dirname, 'converted')));
 
 app.post('/convert', upload.single('image-upload'), (req, res, next) => {
+  
+  sharp(`${uploadsDir}/${req.file.filename}`)
+    .webp({ lossless: true })
+    .toFile(`${convertedDir}/${req.file.filename}.webp`)
+    .then(info => console.log(info))
+    .catch(console.error);
+
   res.redirect(303, `/downloads/${req.file.filename}`);
+
 });
 
 app.get('/', (req, res, next) => {
