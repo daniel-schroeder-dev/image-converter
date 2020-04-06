ddb1fb6926b444ef2ffe9c75cf4fa282091e8130
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-06 14:38:01 -0500

    Add multer to POST /convert. Edit form fields.

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const upload = multer({ dest: 'uploads/' })
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/convert', (req, res, next) => {
-
-  res.send('ok');
+app.post('/convert', upload.single('image'), (req, res, next) => {
+  console.log(req.file.filename);
+  res.redirect(303, '/');
 });
 
 app.get('/', (req, res, next) => {
