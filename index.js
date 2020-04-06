6350d712573e42b8001775338871639e58c837a8
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-06 14:11:34 -0500

    Add ejs boilerplate to render index.ejs.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
 app.listen(8080, () => {
   console.log('Express up at: 8080');
